7147641da6fb55fe9b4c948fd650780949489b94
Peter Boling <peter.boling@gmail.com>
2020-04-30 19:00:12 -0600

    Remove support for mongo_mapper
    mongo_mapper support could be added to a separate plugin gem, but
     - no longer supported
     - depends on old versions of gems mongoid also depends on, e.g. mongo v1.

--- lib/omniauth/identity.rb
+++ lib/omniauth/identity.rb
@@ -10,7 +10,6 @@ module OmniAuth
     autoload :SecurePassword,      'omniauth/identity/secure_password'
     module Models
       autoload :ActiveRecord,      'omniauth/identity/models/active_record'
-      autoload :MongoMapper,       'omniauth/identity/models/mongo_mapper'
       autoload :Mongoid,           'omniauth/identity/models/mongoid'
       autoload :DataMapper,        'omniauth/identity/models/data_mapper'
       autoload :CouchPotatoModule, 'omniauth/identity/models/couch_potato'
