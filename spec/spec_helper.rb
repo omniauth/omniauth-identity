b5bc5d51dc673607fd0cbdddf973d9f728f8576f
Peter Boling <peter.boling@gmail.com>
2025-01-23 22:41:16 +0700

    📝 Fix typos in comments

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,11 +25,11 @@ Dir.glob(Pathname.new(__dir__).join("support/**/", "*.rb")).each { |f| require f
 DEFAULT_PASSWORD = "hang-a-left-at-the-diner"
 DEFAULT_EMAIL = "mojo@example.com"
 
-# Last thing before loading this gem is to setup code coverage
+# Last thing before loading this gem is to set up code coverage
 begin
   # This does not require "simplecov", but
   require "kettle-soup-cover"
-  #   this next line has a side-effect of running `.simplecov`
+  #   this next line has a side effect of running `.simplecov`
   require "simplecov" if defined?(Kettle::Soup::Cover) && Kettle::Soup::Cover::DO_COV
 rescue LoadError
   nil
