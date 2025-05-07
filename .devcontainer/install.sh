7700337df220bcb7a0854fabb9368e712ad21e45
Peter H. Boling <peter.boling@gmail.com>
2025-05-07 19:33:56 +0000

    🔨 Fix devcontainer script

--- .devcontainer/install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-apt-get update -y
-apt-get install -y java postgresql libpq-dev git zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev
