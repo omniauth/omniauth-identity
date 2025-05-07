813c758ce9bf70220427bd71cac442af0eb82e5e
Peter H. Boling <peter.boling@gmail.com>
2025-05-08 03:35:39 +0700

    🔨 Add direnv to devcontainer

--- .devcontainer/apt-install/install.sh
+++ .devcontainer/apt-install/install.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 apt-get update -y
-apt-get install -y default-jdk postgresql libpq-dev git zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev
+apt-get install -y direnv default-jdk postgresql libpq-dev git zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev
+# Adds the direnv setup script to ~/.bashrc file (at the end)
+echo 'eval "$(direnv hook bash)"' >> ~/.bashrc
