e0db833d07c594b4bc93e1328fa16d0d2deaca72
Peter H. Boling <peter.boling@gmail.com>
2026-07-14 13:49:57 -0600

    🔖 Bump gem version

--- lib/omniauth/identity/version.rb
+++ lib/omniauth/identity/version.rb
@@ -3,7 +3,7 @@
 module OmniAuth
   module Identity
     module Version
-      VERSION = "3.2.0"
+      VERSION = "3.2.1"
     end
     VERSION = Version::VERSION # Traditional Constant Location
   end
